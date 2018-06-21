a45b7f046845b84e1df91d4bda22e157cff48003
Adonis Santos <adonisfsantos@gmail.com>
2018-06-21 19:41:17 -0300

    Add files via upload

---
+++ envia_fale.php
@@ -0,0 +1,27 @@
+﻿<?
+
+$destinatario = "adonisfsantos@gmail.com";
+
+$nome = $_REQUEST['nome'];
+$email = $_REQUEST['email'];
+$mensagem = $_REQUEST['mensagem'];
+$assunto = $_REQUEST['assunto'];
+
+// monta o e-mail na variavel $body
+
+$body = "===================================" . "\n";
+$body = $body . "FALE CONOSCO" . "\n";
+$body = $body . "===================================" . "\n\n";
+$body = $body . "Nome: " . $nome . "\n";
+$body = $body . "Email: " . $email . "\n";
+$body = $body . "Mensagem: " . $mensagem . "\n\n";
+$body = $body . "===================================" . "\n";
+
+// envia o email
+mail($destinatario, $assunto , $body, "From: $email\r\n");
+
+// redireciona para a página de obrigado
+header("https://donissantos.github.io/mensagem.html");
+
+
+?>
